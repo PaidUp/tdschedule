f586b83cc7d4acf7bf4cb951483b0c9ce72211d7
Jesse Cogollo <cogollo87@gmail.com>
2015-12-09 16:24:05 -0500

    remove coupon adapter

--- server/api/adapters/schedule.adapter.js
+++ server/api/adapters/schedule.adapter.js
@@ -611,37 +611,3 @@ exports.paymentPlanRetryInformationDelete = function(param, res){
   });
 }
 // payment plan retry information
-
-// coupon (Move to TDCOmmerce)
-/* list
-*/
-exports.couponList = function(res){
-  login(function(err) {
-    if (err) {
-      return res(err);
-    }
-    magento.bighippoCoupon.list(function (err, resPaymentPLanMetadata) {
-      if(err) return res(err);
-      return res(null,camelize(resPaymentPLanMetadata));
-    });
-  });
-}
-
-/* info
-  param =
-  {
-    salesRuleId:'String'
-  }
-*/
-exports.couponInfo = function(param, res){
-  login(function(err) {
-    if (err) {
-      return res(err);
-    }
-    magento.bighippoCoupon.info(param, function (err, resPaymentPLanMetadata) {
-      if(err) return res(err);
-      return res(null,camelize(resPaymentPLanMetadata));
-    });
-  });
-}
-// coupon (Move to TDCOmmerce)
