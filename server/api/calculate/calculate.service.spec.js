308c92e367d0a8216cd0f6b29a12369958adef62
Ricardo Lara <riclara@gmail.com>
2016-02-19 17:46:17 -0600

    add machines to generate prices

---
+++ server/api/calculate/calculate.service.spec.js
@@ -0,0 +1,56 @@
+/**
+ * Created by riclara on 2/19/16.
+ */
+
+'use strict'
+
+const should = require('should')
+const assert = require('chai').assert
+var wagner = require('wagner-core');
+var calculateService = require('./calculate.service')(wagner);
+
+describe('test calculate service' , function(){
+
+    it('calculate' , function(done){
+        calculateService.calculate({
+            originalPrice : 100,
+            stripePercent : 2.9,
+            stripeFlat : 0.30,
+            paidUpFee : 5,
+            discount : 0
+        } , function(err, data){
+            assert.equal(100 , data.owedPrice);
+            assert.equal(91.8 , data.payout);
+            done();
+        });
+    });
+
+    it('calculateProcessing' , function(done){
+        calculateService.calculateProcessing({
+            originalPrice : 100,
+            stripePercent : 2.9,
+            stripeFlat : 0.30,
+            paidUpFee : 5,
+            discount : 0
+        } , function(err, data){
+            //console.log(data)
+            //assert.equal(100 , data.owedPrice);
+            //assert.equal(91.8 , data.payout);
+            done();
+        });
+    });
+
+    it('calculate' , function(done){
+        calculateService.calculateProcessingPaidUp({
+            originalPrice : 100,
+            stripePercent : 2.9,
+            stripeFlat : 0.30,
+            paidUpFee : 5,
+            discount : 0
+        } , function(err, data){
+            assert.equal(103.45 , data.owedPrice);
+            assert.equal(95.15 , data.payout);
+            done();
+        });
+    });
+});
---
+++ server/machines/calculate-prices/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by riclara on 2/19/16.
+ */
+'use strict'
+
+module.exports = {
+    calculate : require('./calculate'),
+    calculateProcessing : require('./calculate-processing'),
+    calculateProcessingPaidUp : require('./calculate-processing-paidup')
+}
