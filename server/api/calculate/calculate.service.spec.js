74ee14ed79ce5e36f86d7349d9a1efa67b578bfb
Ricardo Lara <riclara@gmail.com>
2016-06-08 13:48:48 -0500

    update calculations and test

--- server/api/calculate/calculate.service.spec.js
+++ server/api/calculate/calculate.service.spec.js
@@ -29,20 +29,20 @@ describe('test calculate service' , function(){
 
     it('calculate paidup -- option 1' , function(done){
         calculateService.getPrice({
-            originalPrice : 100,
+            originalPrice : 105,
             stripePercent : 2.9,
             stripeFlat : 0.30,
             paidUpFee : 5,
-            discount : 0,
+            discount : 10,
             payProcessing : false,
             payCollecting : true
         } , function(err, data){
-            assert.deepEqual({ originalPrice: 100,
-                totalFee: 8.35,
-                feePaidUp: 5,
-                feeStripe: 3.35,
-                owedPrice: 105,
-                discount: 0 }
+            assert.deepEqual({ originalPrice: 105,
+                totalFee: 7.54,
+                feePaidUp: 4.50,
+                feeStripe: 3.04,
+                owedPrice: 94.50,
+                discount: 10.50 }
                 , data);
             done();
         });
@@ -54,16 +54,16 @@ describe('test calculate service' , function(){
             stripePercent : 2.9,
             stripeFlat : 0.30,
             paidUpFee : 5,
-            discount : 0,
+            discount : 10,
             payProcessing : false,
             payCollecting : false
         } , function(err, data){
             assert.deepEqual({ originalPrice: 100,
-                    totalFee: 8.2,
-                    feePaidUp: 5,
-                    feeStripe: 3.2,
-                    owedPrice: 100,
-                    discount: 0 }
+                    totalFee: 7.41,
+                    feePaidUp: 4.50,
+                    feeStripe: 2.91,
+                    owedPrice: 90,
+                    discount: 10 }
                 , data);
             done();
         });
@@ -71,20 +71,20 @@ describe('test calculate service' , function(){
 
     it('calculateProcessingPaidUp -- option 3' , function(done){
         calculateService.getPrice({
-            originalPrice : 100,
+            originalPrice : 40,
             stripePercent : 2.9,
             stripeFlat : 0.30,
             paidUpFee : 5,
-            discount : 0,
+            discount : 10,
             payProcessing : true,
             payCollecting : true
         } , function(err, data){
-            assert.deepEqual({ originalPrice: 100,
-                    totalFee: 8.45,
-                    feePaidUp: 5,
-                    feeStripe: 3.45,
-                    owedPrice: 108.45,
-                    discount: 0 }
+            assert.deepEqual({ originalPrice: 40,
+                    totalFee: 2.99,
+                    feePaidUp: 1.65,
+                    feeStripe: 1.34,
+                    owedPrice: 36.00,
+                    discount: 4 }
                 , data);
             done();
         });
@@ -92,20 +92,20 @@ describe('test calculate service' , function(){
 
     it('calculateProcessing option 4' , function(done){
         calculateService.getPrice({
-            originalPrice : 100,
+            originalPrice : 103.29,
             stripePercent : 2.9,
             stripeFlat : 0.30,
             paidUpFee : 5,
-            discount : 0,
+            discount : 10,
             payProcessing : true,
             payCollecting : false
         } , function(err, data){
-            assert.deepEqual({ originalPrice: 100,
-                    totalFee: 8.3,
-                    feePaidUp: 5,
-                    feeStripe: 3.3,
-                    owedPrice: 103.29,
-                    discount: 0 }
+            assert.deepEqual({ originalPrice: 103.29,
+                    totalFee: 7.50,
+                    feePaidUp: 4.50,
+                    feeStripe: 3.00,
+                    owedPrice: 92.96,
+                    discount: 10.33 }
                 , data);
             done();
         });
